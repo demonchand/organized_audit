324e04ddce00febe6a0b9734c31947958a0e231f
Chandra <demonchand@gmail.com>
2016-10-19 09:56:43 +0530

    Stablized Instance and Concern methods

--- lib/organized_audit/model_config.rb
+++ lib/organized_audit/model_config.rb
@@ -5,10 +5,22 @@ module OrganizedAudit
     extend ActiveSupport::Concern
 
     module ClassMethods
-	    def customize_audits(options = {})
-	    	puts "Inside the Customeze Audits"
-	    	# puts options
-	    end
+      def customize_audits(options = {})
+        class_attribute :audit_options, instance_writer: false
+
+        self.audit_options = options
+        puts "Inside the Customeze Audits"
+
+        include OrganizedAudit::ModelConfig::CustInstanceMethods
+      end
+      # extend Audited::Auditor::AuditedClassMethods
+    end
+
+    module CustInstanceMethods
+      def another_method
+        puts "Inside the another method"
+        puts audit_options
+      end
     end
   end
 end
